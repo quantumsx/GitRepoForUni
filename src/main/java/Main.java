85e17d5c4a6e68aa014fc0a23e84c4858200c73d
quantumsx <malow.artem@gmail.com>
2023-11-10 09:40:44 +0400

    func fib(1,1,..) fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,10 +13,10 @@ public class Main {
     }
 
     public static int fib(int n) {
-        if (n == 0) {
+        if (n <= 1) {
             return 0;
         }
-        else if (n == 1) {
+        else if (n == 2) {
             return 1;
         }
         return fib(n - 1) + fib(n - 2);
