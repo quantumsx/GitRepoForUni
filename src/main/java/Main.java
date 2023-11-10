857150aac36e1c132968bd99e84a1c8a7cafef81
quantumsx <malow.artem@gmail.com>
2023-11-10 09:35:24 +0400

    recursive function fib(1,1,..) added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
 
+        //Scanner sc = new Scanner(System.in); Про ввод не совсем понял как реализовать нужно
+        //fib(sc.nextInt()); // Это если пользователь вводит
+
+        System.out.println(fib(10)); // Это если хардкод
+
+    }
+
+    public static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
     }
 }
+
+
+
